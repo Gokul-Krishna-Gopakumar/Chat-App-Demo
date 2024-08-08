ad3a6858394fbe379dd780c02e3f233aa32c1daf
Gokul Krishna Gopakumar <gokulkrishnagopakumar@gmail.com>
2024-08-08 20:38:41 +0530

    Commits on Aug 8th, 2024

---
+++ chat-app-demo/server/Controllers/textController.js
@@ -0,0 +1,23 @@
+const textModel = require("../Models/textModel");
+
+//API for creating message
+const createText = async (req, res) => {
+  const { chatId, senderId, text } = req.body;
+
+  const message = new textModel({
+    chatId,
+    senderId,
+    text,
+  });
+
+  try {
+    const response = await message.save();
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+//get Messages
+
+module.exports = { createText };
